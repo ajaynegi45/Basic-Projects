700385542588648d791a6b89d9723e56b7605c5b
Ajay Negi <97536703+ajaynegi45@users.noreply.github.com>
2023-01-03 19:02:31 +0530

    Update fibonacci.java

--- Recursion/fibonacci.java
+++ Recursion/fibonacci.java
@@ -21,28 +21,26 @@ public class fibonacci {
         System.out.println(a);
         System.out.println(b);
         printfib(0,1,n-2);
-    }
-}
+
 
 
 
         /*  Itration method */
-// public class fibonacci {
-//     public static void main(String[] args) {
+ 
 
-//         int a = 0;
-//         int b = 1;
-//         int n = 7;
-//         System.out.println(a);
-//         System.out.println(b);
+         int a = 0;
+         int b = 1;
+         int n = 7;
+         System.out.println(a);
+         System.out.println(b);
 
-//         for (int i=1;i<=n-2; i++){
-//             int c = a + b ;
-//             System.out.println(c);
-//            a = b;
-//            b = c;
-//         }
+         for (int i=1;i<=n-2; i++){
+             int c = a + b ;
+             System.out.println(c);
+            a = b;
+            b = c;
+         }
 
         
-//     }
-// }
+     }
+}
